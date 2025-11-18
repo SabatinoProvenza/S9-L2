2cdddd4e7c902cb25d14fd4174099ef828c025e1
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-11-18 14:04:48 +0100

    creates navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import MyNav from "./components/MyNav"
 
 function App() {
-  return <></>
+  return (
+    <>
+      <MyNav />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/MyNav.jsx
@@ -0,0 +1,24 @@
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
+
+function MyNav() {
+  return (
+    <Navbar expand="md" className="bg-body-secondary">
+      <Container fluid>
+        <Navbar.Brand href="#">Epibooks</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Brows</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default MyNav
