d00f32496f3b461a74176e0fc4d0379800bcaeb1
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-11-18 16:02:00 +0100

    creates AllTheBooks component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import MyNav from "./components/MyNav"
 import MyFooter from "./components/MyFooter"
+import AllTheBooks from "./components/AllTheBooks"
 
 function App() {
   return (
     <>
       <MyNav />
+      <AllTheBooks />
       <MyFooter />
     </>
   )
---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,31 @@
+import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
+import { Container, Row, Col } from "react-bootstrap"
+import books from "../scifi.json"
+
+function AllTheBooks() {
+  return (
+    <Container className="py-4">
+      <Row className="g-4">
+        {books.map((libro) => (
+          <Col key={libro.asin} sm={12} md={4} lg={3}>
+            <Card style={{ height: "100%" }}>
+              <Card.Img
+                variant="top"
+                style={{ height: "18rem", objectFit: "cover", width: "100%" }}
+                src={libro.img}
+              />
+              <Card.Body className="d-flex flex-column justify-content-end">
+                <Card.Title className="flex-grow-1">{libro.title}</Card.Title>
+                <Card.Text>{libro.price} €</Card.Text>
+                <Button variant="primary">Acquista</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+
+export default AllTheBooks
