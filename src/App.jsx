5cd22a1f896baa598cf84ceb899b38fade5412bb
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-11-18 14:35:51 +0100

    creates myfooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 import MyNav from "./components/MyNav"
+import MyFooter from "./components/MyFooter"
 
 function App() {
   return (
     <>
       <MyNav />
+      <MyFooter />
     </>
   )
 }
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,9 @@
+const MyFooter = function () {
+  return (
+    <footer className="text-center py-3 bg-body-secondary mt-3">
+      <p className="mb-0">Epibooks 2025</p>
+    </footer>
+  )
+}
+
+export default MyFooter
