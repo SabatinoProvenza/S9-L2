f352454203be6e643e98abaf3277c4a456af8b70
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-11-19 14:34:54 +0100

    update single book

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,11 +1,11 @@
 import Card from "react-bootstrap/Card"
 
-function SingleBook(props) {
+function SingleBook({ book }) {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={props.img} alt={props.title} />
+      <Card.Img variant="top" src={book.img} alt={book.title} />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title>{book.title}</Card.Title>
       </Card.Body>
     </Card>
   )
